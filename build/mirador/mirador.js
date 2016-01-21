edc4dede1d040ce7c8e997e516bf387ce1608bf5
Andy McDonald <amcdonald@gmail.com>
2016-01-21 16:12:24 -0500

    build merge

--- build/mirador/mirador.js
+++ build/mirador/mirador.js
@@ -1,5 +1,5 @@
 //! Mirador 0.9.0
-//! Built on 2015-10-14
+//! Built on 2015-11-13
 /*! jQuery UI - v1.10.3 - 2013-06-06
  * http://jqueryui.com
  * Includes: jquery.ui.core.js, jquery.ui.widget.js, jquery.ui.mouse.js, jquery.ui.position.js, jquery.ui.draggable.js, jquery.ui.resizable.js, jquery.ui.button.js, jquery.ui.dialog.js, jquery.ui.menu.js, jquery.ui.slider.js
